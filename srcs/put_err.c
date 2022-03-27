d72af26103d74b0561ca6cb3ea9466ed7e6e6dbe
Takuma Shimizu <tashimiz@student.42tokyo.jp>
2022-03-27 22:58:53 +0900

    [Update] Added 42header for files in src directory

--- srcs/put_err.c
+++ srcs/put_err.c
@@ -1,7 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   put_err.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tashimiz <tashimiz@stdent.42tokyo.jp>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/27 22:50:15 by tashimiz          #+#    #+#             */
+/*   Updated: 2022/03/27 22:50:16 by tashimiz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "pipex.h"
 #include "libft.h"
 
 void	ft_puterr(char *err_msg)
 {
 	ft_putendl_fd(err_msg, STDERR_FILENO);
+}
+
+void	ft_perror(char *prog_name, int errnum)
+{
+	ft_putstr_fd(prog_name, STDERR_FILENO);
+	ft_putstr_fd(": ", STDERR_FILENO);
+	ft_putstr_fd(strerror(errnum), STDERR_FILENO);
+	ft_putstr_fd(": ", STDERR_FILENO);
 }
